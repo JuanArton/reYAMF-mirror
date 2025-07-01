2b46526b312b9aa5f16b32ce9b8d60b1a763f4de
juanarton <juanarton@protonmail.com>
2025-07-01 23:48:42 +0700

    fix: QS New Window app list not appear (removed during rework)

--- app/src/main/java/com/mja/reyamf/manager/services/QSNewWindowService.kt
+++ app/src/main/java/com/mja/reyamf/manager/services/QSNewWindowService.kt
@@ -1,13 +1,15 @@
 package com.mja.reyamf.manager.services
 
+import android.content.Intent
 import android.service.quicksettings.TileService
 import androidx.preference.PreferenceManager
+import com.mja.reyamf.manager.applist.AppListWindow
 
 class QSNewWindowService : TileService() {
     override fun onClick() {
         super.onClick()
         if (PreferenceManager.getDefaultSharedPreferences(this).getBoolean("useAppList", true))
-            //YAMFManagerProxy.openAppList() //TODO: Apply in userspace
+            startService(Intent(this, AppListWindow::class.java))
         else YAMFManagerProxy.createWindow()
     }
 }
